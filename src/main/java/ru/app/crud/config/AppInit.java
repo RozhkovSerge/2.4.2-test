45b651ef67cb2308c8abb46010997e7e841dc8cf
Serge Rozkov <rozkov_ss@mail.ru>
2021-11-26 13:12:43 +0300

    cyrillic issue resolved

--- src/main/java/ru/app/crud/config/AppInit.java
+++ src/main/java/ru/app/crud/config/AppInit.java
@@ -1,6 +1,12 @@
 package ru.app.crud.config;
 
+import org.springframework.web.filter.CharacterEncodingFilter;
+import org.springframework.web.filter.HiddenHttpMethodFilter;
 import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
+
+import javax.servlet.FilterRegistration;
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
 
 public class AppInit extends AbstractAnnotationConfigDispatcherServletInitializer {
     @Override
@@ -10,11 +16,21 @@ public class AppInit extends AbstractAnnotationConfigDispatcherServletInitialize
 
     @Override
     protected Class<?>[] getServletConfigClasses() {
-        return new Class[] {ru.app.crud.config.WebConfig.class};
+        return new Class[]{ru.app.crud.config.WebConfig.class};
     }
 
     @Override
     protected String[] getServletMappings() {
-        return new String[] {"/"};
+        return new String[]{"/"};
+    }
+
+    @Override
+    public void onStartup(ServletContext servletContext) throws ServletException {
+        super.onStartup(servletContext);
+        FilterRegistration.Dynamic filterRegistration = servletContext.addFilter("characterEncodingFilter",
+                new CharacterEncodingFilter("UTF-8", true, true));
+        filterRegistration.addMappingForUrlPatterns(null, false, "/*");
+        filterRegistration = servletContext.addFilter("hiddenHttpMethodFilter", new HiddenHttpMethodFilter());
+        filterRegistration.addMappingForUrlPatterns(null, false, "/*");
     }
 }
